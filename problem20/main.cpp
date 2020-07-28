2d20744a781892a0941b2bdb230133e924ceee63
NamanMathur77 <namanmathur619@gmail.com>
2020-07-28 09:53:52 +0530

    sum of two values CSES

---
+++ problem20/main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ii = tuple<long, long>;
+typedef long long ll;
+int main(){
+	// #ifndef ONLINE_JUDGE
+	// freopen("/home/naman/CP/problem20/input.txt", "r", stdin);
+	// freopen("/home/naman/CP/problem20/output.txt", "w", stdout);
+	// #endif
+	ll n,x;
+	cin>>n>>x;
+	vector<ii> v;
+	for(ll i=0;i<n;i++){
+		ll a;
+		cin>>a;
+		v.push_back({a,i});
+	}
+	sort(v.begin(),v.end());
+	ll i=0;
+	ll j=n-1;
+	while(i<j){
+		if(get<0>(v[i])+get<0>(v[j])==x){
+			cout<<get<1>(v[i])+1<<" "<<get<1>(v[j])+1;
+			return 0;
+		}
+		if(get<0>(v[i])+get<0>(v[j])<x){
+			i+=1;
+		}
+		else{
+			j-=1;
+		}
+	}
+	cout<<"IMPOSSIBLE\n";
+
+}
